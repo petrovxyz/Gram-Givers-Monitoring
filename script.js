43eef203d2796619537e40138ed7944fb318b565
petrovxyz <petrovxyz@proton.me>
2024-02-15 00:53:02 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,294 @@
+const wallets = {
+    extraSmall: [
+        { 
+            url: 'https://tonviewer.com/EQCfwe95AJDfKuAoP1fBtu-un1yE7Mov-9BXaFM3lrJZwqg_/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 350000000,
+            id: 'Grenei'
+        },
+        { 
+            url: 'https://tonviewer.com/EQBoATvbIa9vA7y8EUQE4tlsrrt0EhSUK4mndp49V0z7Me3M/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 350000000,
+            id: 'Bevro'
+        },
+        { 
+            url: 'https://tonviewer.com/EQAV3tsPXau3VJanBw4KCFaMk3l_n3sX8NHZNgICFrR-9EGE/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 350000000,
+            id: 'Bragie'
+        },
+        { 
+            url: 'https://tonviewer.com/EQAR9DvLZMHo9FAVMHI1vHvL7Fi7jWgjKtUARZ2S_nopQRYz/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 350000000,
+            id: 'Ovyt'
+        },
+        { 
+            url: 'https://tonviewer.com/EQC10L__G2SeEeM2Lw9osGyYxhoIPqJwE-8Pe7728JcmnJzW/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 350000000,
+            id: 'Elzrir'
+        },
+        { 
+            url: 'https://tonviewer.com/EQDZJFkh12kw-zLGqKSGVDf1V2PRzedGZDFDcFml5_0QerST/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 350000000,
+            id: 'Orsot'
+        },
+        { 
+            url: 'https://tonviewer.com/EQCiLN0gEiZqthGy-dKl4pi4kqWJWjRzR3Jv4jmPOtQHveDN/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 350000000,
+            id: 'Brandred'
+        },
+        { 
+            url: 'https://tonviewer.com/EQDB8Mo9EviBkg_BxfNv6C2LO_foJRXcgEF41pmQvMvnB9Jn/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 350000000,
+            id: 'Gotontom'
+        },
+        { 
+            url: 'https://tonviewer.com/EQAidDzp6v4oe-vKFWvsV8MQzY-4VaeUFnGM3ImrKIJUIid9/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 350000000,
+            id: 'Gridrurus'
+        },
+        { 
+            url: 'https://tonviewer.com/EQAFaPmLLhXveHcw3AYIGDlHbGAbfQWlH45WGf4K4D6DNZxY/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 350000000,
+            id: 'Goithaysdy'
+        },
+    ],
+    small: [
+        { 
+            url: 'https://tonviewer.com/EQDSGvoktoIRTL6fBEK_ysS8YvLoq3cqW2TxB_xHviL33ex2/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 75000000,
+            id: 'Zordag'
+        },
+        { 
+            url: 'https://tonviewer.com/EQCvMmHhSYStEtUAEDrpV39T2GWl-0K-iqCxSSZ7I96L4yow/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 75000000,
+            id: 'Diergy'
+        },
+        { 
+            url: 'https://tonviewer.com/EQBvumwjKe7xlrjc22p2eLGT4UkdRnrmqmcEYT94J6ZCINmt/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 75000000,
+            id: 'Graghais'
+        },
+        { 
+            url: 'https://tonviewer.com/EQDEume45yzDIdSy_Cdz7KIKZk0HyCFIr0yKdbtMyPfFUkbl/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 75000000,
+            id: 'Froiddry'
+        },
+        { 
+            url: 'https://tonviewer.com/EQAO7jXcX-fJJZl-kphbpdhbIDUqcAiYcAr9RvVlFl38Uatt/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 75000000,
+            id: 'Zovraim'
+        },
+        { 
+            url: 'https://tonviewer.com/EQAvheS_G-U57CE55UlwF-3M-cc4cljbLireYCmAMe_RHWGF/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 75000000,
+            id: 'Ytynth'
+        },
+        { 
+            url: 'https://tonviewer.com/EQCba5q9VoYGgiGykVazOUZ49UK-1RljUeZgU6E-bW0bqF2Z/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 75000000,
+            id: 'Guvanar'
+        },
+        { 
+            url: 'https://tonviewer.com/EQCzT8Pk1Z_aMpNukdV-Mqwc6LNaCNDt-HD6PiaSuEeCD0hV/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 75000000,
+            id: 'Omecra'
+        },
+        { 
+            url: 'https://tonviewer.com/EQDglg3hI89dySlr-FR_d1GQCMirkLZH6TPF-NeojP-DbSgY/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 75000000,
+            id: 'Nygasdot'
+        },
+        { 
+            url: 'https://tonviewer.com/EQDIDs45shbXRwhnXoFZg303PkG2CihbVvQXw1k0_yVIqxcA/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 75000000,
+            id: 'Jodrog'
+        },
+    ],
+    medium: [
+        { 
+            url: 'https://tonviewer.com/EQD7VspHSNS4VSpN7QQicNgSYoJ68CmdC6oL5ZEKHSXe26Sa/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 50000000,
+            id: 'Dyrso'
+        },
+        { 
+            url: 'https://tonviewer.com/EQC5uEgW0MkTbCRBZB72maxCZT3m14OK2FcSLVr2H_7MTTSF/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 50000000,
+            id: 'Dodrig'
+        },
+        { 
+            url: 'https://tonviewer.com/EQC2nD9nQNRhcfWhdBzRK-wdlTO4hGxnPFzdSxKN777tab2_/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 50000000,
+            id: 'Jardenth'
+        },
+        { 
+            url: 'https://tonviewer.com/EQAqd4vV0O5oGfA7bl6fVORD_Y4PTNZG82AC2BObBux51g2w/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 50000000,
+            id: 'Emmur'
+        },
+        { 
+            url: 'https://tonviewer.com/EQDcOxqaWgEhN_j6Tc4iIQNCj2dBf9AFm0S9QyouwifYo9KD/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 50000000,
+            id: 'Raldre'
+        },
+        { 
+            url: 'https://tonviewer.com/EQAjYs4-QKve9gtwC_HrKNR0Eaqhze4sKUmRhRYeensX8iu3/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 50000000,
+            id: 'Kalriden'
+        },
+        { 
+            url: 'https://tonviewer.com/EQBGhm8bNil8tw4Z2Ekk4sKD-vV-LCz7BW_qIYCEjZpiMF6Q/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 50000000,
+            id: 'Zynnorris'
+        },
+        { 
+            url: 'https://tonviewer.com/EQCtrloCD9BHbVT7q8aXkh-JtL_ZDvtJ5Y-eF2ahg1Ru1EUl/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 50000000,
+            id: 'Jyrrydoss'
+        },
+        { 
+            url: 'https://tonviewer.com/EQCWMIUBrpwl7OeyEQsOF9-ZMKCQ7fh3_UOvM2N5y77u8uPc/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 50000000,
+            id: 'Celbyss'
+        },
+        { 
+            url: 'https://tonviewer.com/EQD_71XLqY8nVSf4i5pqGsCjz6EUo2kQEEQq0LUAgg6AHolO/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 50000000,
+            id: 'Qokos'
+        },
+    ],
+    large: [
+        { 
+            url: 'https://tonviewer.com/EQDUIeTNcRUqsz4bugyWl4q4vg16PN2EwiyyAIEbf7_WJZZH/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 25000000,
+            id: 'Bavaid'
+        },
+        { 
+            url: 'https://tonviewer.com/EQC4qKAIM0Od4RFG-4MAY0dJ3j4Wrcs0jc1XeWKJURYB9KSz/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 25000000,
+            id: 'Rezam'
+        },
+        { 
+            url: 'https://tonviewer.com/EQC0Ssi1gl0IQKEGsCp91NeiTThdMqCzYvlX9sVLEU97rWqL/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 25000000,
+            id: 'Roilbo'
+        },
+        { 
+            url: 'https://tonviewer.com/EQDO2_2zkIJPqBKeE_P1VvDYOJi1vGPgiKo0Aa6Z-bY7BeuG/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 25000000,
+            id: 'Ammass'
+        },
+        { 
+            url: 'https://tonviewer.com/EQADEy4zcVl-ADNMISdYSs5cVjJcHmwC9_phXXjqNKgME7j6/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 25000000,
+            id: 'Myron'
+        },
+        { 
+            url: 'https://tonviewer.com/EQDWELx3CYohD9sIjhyEIhP915kL_3XthqruCbmcB0YTWDqQ/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 25000000,
+            id: 'Gairris'
+        },
+        { 
+            url: 'https://tonviewer.com/EQDdoh2hzGFHdQtiXJNPNrwA8yIGd4-sFxyuEr3z6JL5BIFi/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 25000000,
+            id: 'Frizag'
+        },
+        { 
+            url: 'https://tonviewer.com/EQALXKp6G-IjWTPEqFKILkqcql-43DcoPzJ21Z02abpBPaQK/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 25000000,
+            id: 'Chamaicross'
+        },
+        { 
+            url: 'https://tonviewer.com/EQBAHXFxs1ohHY2bzW9A-V0NDznkFlROkNF_oyppxlLfsyEJ/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 25000000,
+            id: 'Chervecrit'
+        },
+        { 
+            url: 'https://tonviewer.com/EQCUwgBew9u4NwwuFsfPsXX9a69K55uFcieaHtc-c37OYDJO/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            maxBalance: 25000000,
+            id: 'Edrocrurth'
+        },
+    ],
+};
+
+async function getWalletBalance(walletURL) {
+    try {
+        const response = await fetch(walletURL);
+        const html = await response.text();
+        
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, 'text/html');
+        
+        const balanceElement = doc.querySelector('.t1gmfwv5.n1uax993.b1l85tie'); 
+        if (balanceElement) {
+            const balanceText = balanceElement.textContent.trim().replace(/[^0-9.,]/g, ''); 
+            const balance = parseFloat(balanceText.replace(/,/g, '')); 
+            return balance;
+        } else {
+            throw new Error('Баланс не найден на странице');
+        }
+    } catch (error) {
+        console.error('Ошибка при парсинге баланса:', error.message);
+        return null;
+    }
+}
+
+async function updateWallet(wallet) {
+    try {
+        const balance = await getWalletBalance(wallet.url);
+        const remainingBalance = wallet.maxBalance;
+        const progressBarWidth = (balance / wallet.maxBalance) * 100;
+
+        const walletElement = document.getElementById(wallet.url);
+        if (walletElement) {
+            walletElement.querySelector('.balance').textContent = `Remaining: ${balance.toLocaleString()}`;
+            walletElement.querySelector('.remaining').textContent = `Сapacity: ${remainingBalance.toLocaleString()}`;
+            walletElement.querySelector('.status-fill').style.width = `${progressBarWidth}%`; 
+        } else {
+            console.error('Элемент кошелька не найден:', wallet.url);
+        }
+    } catch (error) {
+        console.error('Ошибка при обновлении информации о кошельке:', error.message);
+    }
+}
+
+async function updateWallets() {
+    try {
+        for (const category in wallets) {
+            for (const wallet of wallets[category]) {
+                await updateWallet(wallet);
+            }
+        }
+    } catch (error) {
+        console.error('Ошибка при обновлении информации о кошельках:', error.message);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+    try {
+        for (const category in wallets) {
+            const categoryElement = document.getElementById(`${category}Wallets`);
+            for (const wallet of wallets[category]) {
+                const walletElement = document.createElement('div');
+                walletElement.classList.add('wallet');
+                walletElement.id = wallet.url; 
+                walletElement.innerHTML = `
+                <!-- HTML -->
+                <div class="wallet-card" id="${wallet.url}">
+                    <div class="wallet-info">
+                        <p><i class="bi bi-box"></i><b> Giver ${wallet.id}</b></p><a href="${wallet.url}">Tonviewer</a>
+                        <p class="balance">Remaining: loading...</p>
+                        <p class="remaining">Сapacity: loading...</p>
+                    </div>
+                    <div class="status-bar">
+                        <div class="status-fill"></div>
+                    </div>
+                </div>                
+                `;
+                categoryElement.appendChild(walletElement);
+            }
+        }
+    } catch (error) {
+        console.error('Ошибка при инициализации страницы:', error.message);
+    }
+    
+    setInterval(updateWallets, 5000);
+});
