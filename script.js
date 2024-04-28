5d18d7130b375d14ce8ffde99bb20eb0f5c6c954
petrovxyz <petrovxyz@proton.me>
2024-04-28 16:53:30 +0300

    tonviewer -> tonscan

--- script.js
+++ script.js
@@ -1,61 +1,61 @@
 const wallets = {
     extraSmall: [
         { 
-            tonviewer: 'https://tonviewer.com/EQCfwe95AJDfKuAoP1fBtu-un1yE7Mov-9BXaFM3lrJZwqg_/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQDz5ABKpaMO8BunzgNpcG8TAHvleDXGjSqIi6nOf5cLsFKg',
             url: 'EQDz5ABKpaMO8BunzgNpcG8TAHvleDXGjSqIi6nOf5cLsFKg',
             maxBalance: 350000000,
             id: 'Grenei'
         },
         { 
-            tonviewer: 'https://tonviewer.com/EQBoATvbIa9vA7y8EUQE4tlsrrt0EhSUK4mndp49V0z7Me3M/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQB0wM5SSGxOMQW1kiA_CSnZi8xONhr2EaH0-DH3ErshW_kT',
             url: 'EQB0wM5SSGxOMQW1kiA_CSnZi8xONhr2EaH0-DH3ErshW_kT',
             maxBalance: 350000000,
             id: 'Bevro'
         },
         { 
-            tonviewer: 'https://tonviewer.com/EQAV3tsPXau3VJanBw4KCFaMk3l_n3sX8NHZNgICFrR-9EGE/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQDifNns50Ial2wezfhHT69ycmucPjQvbVa8qLsbFoVqN9_-',
             url: 'EQDifNns50Ial2wezfhHT69ycmucPjQvbVa8qLsbFoVqN9_-',
             maxBalance: 350000000,
             id: 'Bragie'
         },
         { 
-            tonviewer: 'https://tonviewer.com/EQAR9DvLZMHo9FAVMHI1vHvL7Fi7jWgjKtUARZ2S_nopQRYz/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQDMI8s3OcoaM5A9OhIOj8lh1jqGVoJc4rouNi1XPIu5YcXh',
             url: 'EQDMI8s3OcoaM5A9OhIOj8lh1jqGVoJc4rouNi1XPIu5YcXh',
             maxBalance: 350000000,
             id: 'Ovyt'
         },
         { 
-            tonviewer: 'https://tonviewer.com/EQC10L__G2SeEeM2Lw9osGyYxhoIPqJwE-8Pe7728JcmnJzW/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQCGGM9fF9hmvAt7OLWZKbX0tf3A5ZrCF7EW5mmdUk3wkqGm',
             url: 'EQCGGM9fF9hmvAt7OLWZKbX0tf3A5ZrCF7EW5mmdUk3wkqGm',
             maxBalance: 350000000,
             id: 'Elzrir'
         },
         { 
-            tonviewer: 'https://tonviewer.com/EQDZJFkh12kw-zLGqKSGVDf1V2PRzedGZDFDcFml5_0QerST/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQCh7TvrKTFF1shor0ni2SsDF2poyGltxgWaJYOY6J3kqi1G',
             url: 'EQCh7TvrKTFF1shor0ni2SsDF2poyGltxgWaJYOY6J3kqi1G',
             maxBalance: 350000000,
             id: 'Orsot'
         },
         { 
-            tonviewer: 'https://tonviewer.com/EQCiLN0gEiZqthGy-dKl4pi4kqWJWjRzR3Jv4jmPOtQHveDN/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQB3zmSjxPn2c-EcbDAzYiPZtbPi8DkxdcnjHYKRG3xealN0',
             url: 'EQB3zmSjxPn2c-EcbDAzYiPZtbPi8DkxdcnjHYKRG3xealN0',
             maxBalance: 350000000,
             id: 'Brandred'
         },
         { 
-            tonviewer: 'https://tonviewer.com/EQDB8Mo9EviBkg_BxfNv6C2LO_foJRXcgEF41pmQvMvnB9Jn/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQBFUICbJRNPrV1Q6RN12oq7TNc87hkyV_8stbQOn5Q0ddd2',
             url: 'EQBFUICbJRNPrV1Q6RN12oq7TNc87hkyV_8stbQOn5Q0ddd2',
             maxBalance: 350000000,
             id: 'Gotontom'
         },
         { 
-            tonviewer: 'https://tonviewer.com/EQAidDzp6v4oe-vKFWvsV8MQzY-4VaeUFnGM3ImrKIJUIid9/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQA-9Z_fJ3PstFkCoZIMij9xM3SVEJfqYtv-x8tKfUlb0Bh5',
             url: 'EQA-9Z_fJ3PstFkCoZIMij9xM3SVEJfqYtv-x8tKfUlb0Bh5',
             maxBalance: 350000000,
             id: 'Gridrurus'
         },
         { 
-            tonviewer: 'https://tonviewer.com/EQAFaPmLLhXveHcw3AYIGDlHbGAbfQWlH45WGf4K4D6DNZxY/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQDKx_EepIPaTIBiaCLbyQn23cyxHQ2UhZxZWf34AKi-MrPN',
             url: 'EQDKx_EepIPaTIBiaCLbyQn23cyxHQ2UhZxZWf34AKi-MrPN',
             maxBalance: 350000000,
             id: 'Goithaysdy'
@@ -63,13 +63,13 @@ const wallets = {
     ],
     small: [
         { 
-            tonviewer: 'https://tonviewer.com/EQDSGvoktoIRTL6fBEK_ysS8YvLoq3cqW2TxB_xHviL33ex2/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQAdbzmwgiGzoO8Qd9TaXez1_glc3u5k5OhyVP19eQU6_w8u',
             url: 'EQAdbzmwgiGzoO8Qd9TaXez1_glc3u5k5OhyVP19eQU6_w8u',
             maxBalance: 75000000,
             id: 'Zordag'
         },
         { 
-            tonviewer: 'https://tonviewer.com/EQCvMmHhSYStEtUAEDrpV39T2GWl-0K-iqCxSSZ7I96L4yow/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQDAsvqI2NjvIQRgBZlJlLgE1O_qCbhLF0JHJaCacN5JYqlh',
             url: 'EQDAsvqI2NjvIQRgBZlJlLgE1O_qCbhLF0JHJaCacN5JYqlh',
             maxBalance: 75000000,
             id: 'Diergy'
@@ -81,43 +81,43 @@ const wallets = {
         },
         */
         { 
-            tonviewer: 'https://tonviewer.com/EQDEume45yzDIdSy_Cdz7KIKZk0HyCFIr0yKdbtMyPfFUkbl/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQBt17w56e3ZJTl2KlVOt8sNYecHHhOOfM2n4G7vDSlh5bpi',
             url: 'EQBt17w56e3ZJTl2KlVOt8sNYecHHhOOfM2n4G7vDSlh5bpi',
             maxBalance: 75000000,
             id: 'Froiddry'
         },
         { 
-            tonviewer: 'https://tonviewer.com/EQAO7jXcX-fJJZl-kphbpdhbIDUqcAiYcAr9RvVlFl38Uatt/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQAVYeKiun0grXNECt2A-m3N9jmX4eLFazefhmpfATZv_3nd',
             url: 'EQAVYeKiun0grXNECt2A-m3N9jmX4eLFazefhmpfATZv_3nd',
             maxBalance: 75000000,
             id: 'Zovraim'
         },
         { 
-            tonviewer: 'https://tonviewer.com/EQAvheS_G-U57CE55UlwF-3M-cc4cljbLireYCmAMe_RHWGF/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQCGGMVDNldf8rEk7Kes5lylUrONdXq9qSg2OO2u-fC-6AHP',
             url: 'EQCGGMVDNldf8rEk7Kes5lylUrONdXq9qSg2OO2u-fC-6AHP',
             maxBalance: 75000000,
             id: 'Ytynth'
         },
         { 
-            tonviewer: 'https://tonviewer.com/EQCba5q9VoYGgiGykVazOUZ49UK-1RljUeZgU6E-bW0bqF2Z/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQCmEkx3VXRdbkvqs6qic91cxphZsPS2vAUvN70qWymbKa_a',
             url: 'EQCmEkx3VXRdbkvqs6qic91cxphZsPS2vAUvN70qWymbKa_a',
             maxBalance: 75000000,
             id: 'Guvanar'
         },
         { 
-            tonviewer: 'https://tonviewer.com/EQCzT8Pk1Z_aMpNukdV-Mqwc6LNaCNDt-HD6PiaSuEeCD0hV/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQBQ9V9FSSnbG180zmshd7jDgqiUiCXH-oYerS1-Z66juHBd',
             url: 'EQBQ9V9FSSnbG180zmshd7jDgqiUiCXH-oYerS1-Z66juHBd',
             maxBalance: 75000000,
             id: 'Omecra'
         },
         { 
-            tonviewer: 'https://tonviewer.com/EQDglg3hI89dySlr-FR_d1GQCMirkLZH6TPF-NeojP-DbSgY/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQBLstaZ2tad5CSg-uVP76fwvj_K8SBrkOvLKP7pCEYAmHFY',
             url: 'EQBLstaZ2tad5CSg-uVP76fwvj_K8SBrkOvLKP7pCEYAmHFY',
             maxBalance: 75000000,
             id: 'Nygasdot'
         },
         {
-            tonviewer: 'https://tonviewer.com/EQDglg3hI89dySlr-FR_d1GQCMirkLZH6TPF-NeojP-DbSgY/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQC7FvnqMqEltKvFyuGiNnfJLh-d8Ej0HieRp73l0NoyJtj4',
             url: 'EQC7FvnqMqEltKvFyuGiNnfJLh-d8Ej0HieRp73l0NoyJtj4',
             maxBalance: 75000000,
             id: 'Jodrog'
@@ -125,61 +125,61 @@ const wallets = {
     ],
     medium: [
         { 
-            tonviewer: 'https://tonviewer.com/EQD7VspHSNS4VSpN7QQicNgSYoJ68CmdC6oL5ZEKHSXe26Sa/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQDuVMbAnnQr9Ecxd2sDIc5TDjgRgcFCLyL4sEOLUuTAYDEO',
             url: 'EQDuVMbAnnQr9Ecxd2sDIc5TDjgRgcFCLyL4sEOLUuTAYDEO',
             maxBalance: 50000000,
             id: 'Dyrso'
         },
         { 
-            tonviewer: 'https://tonviewer.com/EQC5uEgW0MkTbCRBZB72maxCZT3m14OK2FcSLVr2H_7MTTSF/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQCYlzcaWPnHZu2koCLyXh9Jw9wQwRbqaqNIw2bhkfQaFdW8',
             url: 'EQCYlzcaWPnHZu2koCLyXh9Jw9wQwRbqaqNIw2bhkfQaFdW8',
             maxBalance: 50000000,
             id: 'Dodrig'
         },
         { 
-            tonviewer: 'https://tonviewer.com/EQC2nD9nQNRhcfWhdBzRK-wdlTO4hGxnPFzdSxKN777tab2_/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQBD5hih9xsfWEN41tJPMvabiLaz1jh86KkM9nKG3hOq1DO-',
             url: 'EQBD5hih9xsfWEN41tJPMvabiLaz1jh86KkM9nKG3hOq1DO-',
             maxBalance: 50000000,
             id: 'Jardenth'
         },
         { 
-            tonviewer: 'https://tonviewer.com/EQAqd4vV0O5oGfA7bl6fVORD_Y4PTNZG82AC2BObBux51g2w/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQBuGoJmb8aUOMd0yt4pkevSY1WEWGyHyQkyTMOZZw4fnZhS',
             url: 'EQBuGoJmb8aUOMd0yt4pkevSY1WEWGyHyQkyTMOZZw4fnZhS',
             maxBalance: 50000000,
             id: 'Emmur'
         },
         { 
-            tonviewer: 'https://tonviewer.com/EQDcOxqaWgEhN_j6Tc4iIQNCj2dBf9AFm0S9QyouwifYo9KD/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQBKkIbu4suW_V8yzB3hBqDXOD_fe7w4jOWpf0231XSNMmC7',
             url: 'EQBKkIbu4suW_V8yzB3hBqDXOD_fe7w4jOWpf0231XSNMmC7',
             maxBalance: 50000000,
             id: 'Raldre'
         },
         { 
-            tonviewer: 'https://tonviewer.com/EQAjYs4-QKve9gtwC_HrKNR0Eaqhze4sKUmRhRYeensX8iu3/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQD5VUfl0vs24Pcf1dj6LV5La7prfn1lbzhrcgQ33lm5v1zM',
             url: 'EQD5VUfl0vs24Pcf1dj6LV5La7prfn1lbzhrcgQ33lm5v1zM',
             maxBalance: 50000000,
             id: 'Kalriden'
         },
         { 
-            tonviewer: 'https://tonviewer.com/EQBGhm8bNil8tw4Z2Ekk4sKD-vV-LCz7BW_qIYCEjZpiMF6Q/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQBwKfqIlQ3HkDuS1MiNlUc0h77xYL1N7YeowU49sDwnwDfU',
             url: 'EQBwKfqIlQ3HkDuS1MiNlUc0h77xYL1N7YeowU49sDwnwDfU',
             maxBalance: 50000000,
             id: 'Zynnorris'
         },
         { 
-            tonviewer: 'https://tonviewer.com/EQCtrloCD9BHbVT7q8aXkh-JtL_ZDvtJ5Y-eF2ahg1Ru1EUl/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQBp8dVtgqMOdFmo4zF0Q3S8p37M4riJ4HKDWN3f4kzpbSjD',
             url: 'EQBp8dVtgqMOdFmo4zF0Q3S8p37M4riJ4HKDWN3f4kzpbSjD',
             maxBalance: 50000000,
             id: 'Jyrrydoss'
         },
         { 
-            tonviewer: 'https://tonviewer.com/EQCWMIUBrpwl7OeyEQsOF9-ZMKCQ7fh3_UOvM2N5y77u8uPc/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQC9MrbOn_RAbzBqPbm9OZmDOZipE_F9_DyAjBlRnZnzpRTu',
             url: 'EQC9MrbOn_RAbzBqPbm9OZmDOZipE_F9_DyAjBlRnZnzpRTu',
             maxBalance: 50000000,
             id: 'Celbyss'
         },
         { 
-            tonviewer: 'https://tonviewer.com/EQD_71XLqY8nVSf4i5pqGsCjz6EUo2kQEEQq0LUAgg6AHolO/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQAn8xg-0ertl0ATCSeKdywVXHrXyjv3gRg6ofy83IEmmv5T',
             url: 'EQAn8xg-0ertl0ATCSeKdywVXHrXyjv3gRg6ofy83IEmmv5T',
             maxBalance: 50000000,
             id: 'Qokos'
@@ -187,61 +187,61 @@ const wallets = {
     ],
     large: [
         { 
-            tonviewer: 'https://tonviewer.com/EQDUIeTNcRUqsz4bugyWl4q4vg16PN2EwiyyAIEbf7_WJZZH/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQAHayh7282LwrdvEN9DpVgSzK_Z4lgNsbFRtEehiV0Bjy8D',
             url: 'EQAHayh7282LwrdvEN9DpVgSzK_Z4lgNsbFRtEehiV0Bjy8D',
             maxBalance: 25000000,
             id: 'Bavaid'
         },
         { 
-            tonviewer: 'https://tonviewer.com/EQC4qKAIM0Od4RFG-4MAY0dJ3j4Wrcs0jc1XeWKJURYB9KSz/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQCg7Bjedg1TXm4zzwt1-_YgaCjzSdxXrNzHTv0978eaHEPL',
             url: 'EQCg7Bjedg1TXm4zzwt1-_YgaCjzSdxXrNzHTv0978eaHEPL',
             maxBalance: 25000000,
             id: 'Rezam'
         },
         { 
-            tonviewer: 'https://tonviewer.com/EQC0Ssi1gl0IQKEGsCp91NeiTThdMqCzYvlX9sVLEU97rWqL/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQBnt4TnEunxoTnbVFN3i1UcNFYH4y3NnLha7i0BwGFpfR0d',
             url: 'EQBnt4TnEunxoTnbVFN3i1UcNFYH4y3NnLha7i0BwGFpfR0d',
             maxBalance: 25000000,
             id: 'Roilbo'
         },
         { 
-            tonviewer: 'https://tonviewer.com/EQDO2_2zkIJPqBKeE_P1VvDYOJi1vGPgiKo0Aa6Z-bY7BeuG/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQCiMbkesO1ky9lnfFcA3oc8W8DUrd2Giv1Jx9EsL9lifYVV',
             url: 'EQCiMbkesO1ky9lnfFcA3oc8W8DUrd2Giv1Jx9EsL9lifYVV',
             maxBalance: 25000000,
             id: 'Ammass'
         },
         { 
-            tonviewer: 'https://tonviewer.com/EQADEy4zcVl-ADNMISdYSs5cVjJcHmwC9_phXXjqNKgME7j6/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQBpPJW0LD6JWBdfPbRZjdlO2oduMNV9DiTSvnvUbL5ZbD_S',
             url: 'EQBpPJW0LD6JWBdfPbRZjdlO2oduMNV9DiTSvnvUbL5ZbD_S',
             maxBalance: 25000000,
             id: 'Myron'
         },
         { 
-            tonviewer: 'https://tonviewer.com/EQDWELx3CYohD9sIjhyEIhP915kL_3XthqruCbmcB0YTWDqQ/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQAMYy1yijpkHX9yqMvSSr2CeIEiQRuhy50EoCHTMo4gaGY1',
             url: 'EQAMYy1yijpkHX9yqMvSSr2CeIEiQRuhy50EoCHTMo4gaGY1',
             maxBalance: 25000000,
             id: 'Gairris'
         },
         { 
-            tonviewer: 'https://tonviewer.com/EQDdoh2hzGFHdQtiXJNPNrwA8yIGd4-sFxyuEr3z6JL5BIFi/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQCakdHQb-6O89M65VFB9ryN637s3P0-uk8-yZ4WGVEsu-He',
             url: 'EQCakdHQb-6O89M65VFB9ryN637s3P0-uk8-yZ4WGVEsu-He',
             maxBalance: 25000000,
             id: 'Frizag'
         },
         { 
-            tonviewer: 'https://tonviewer.com/EQALXKp6G-IjWTPEqFKILkqcql-43DcoPzJ21Z02abpBPaQK/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQA8g2QX0WGIb3Papznpo1BoFrmYRoCp5NsLQXP-o8qMI95I',
             url: 'EQA8g2QX0WGIb3Papznpo1BoFrmYRoCp5NsLQXP-o8qMI95I',
             maxBalance: 25000000,
             id: 'Chamaicross'
         },
         { 
-            tonviewer: 'https://tonviewer.com/EQBAHXFxs1ohHY2bzW9A-V0NDznkFlROkNF_oyppxlLfsyEJ/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQA_FZx8tpyPyAN6ZXssaAh3vqReoDe3MdCar7aj_9Ixe6tP',
             url: 'EQA_FZx8tpyPyAN6ZXssaAh3vqReoDe3MdCar7aj_9Ixe6tP',
             maxBalance: 25000000,
             id: 'Chervecrit'
         },
         { 
-            tonviewer: 'https://tonviewer.com/EQCUwgBew9u4NwwuFsfPsXX9a69K55uFcieaHtc-c37OYDJO/jetton/EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O',
+            tonscan: 'https://tonscan.org/address/EQCHhMzHF_mh1fiahPoXPuyWuohpUtMwVorD-zpvxfgmsq0J',
             url: 'EQCHhMzHF_mh1fiahPoXPuyWuohpUtMwVorD-zpvxfgmsq0J',
             maxBalance: 25000000,
             id: 'Edrocrurth'
@@ -316,7 +316,9 @@ document.addEventListener('DOMContentLoaded', async () => {
                     <div class="wallet-info">
                         <p><i class="bi bi-box"></i><b> Giver ${wallet.id}</b></p>
                         <span>
-                        <span class="tonviewer" data-url="${wallet.tonviewer}" data-target="_blank">Tonviewer</span>
+                        <a href="${wallet.tonscan}" target="_blank" class="tonscan">
+                        tonscan
+                        </a>
                         </span>
                         <p class="balance">Remaining: loading...</p>
                         <p class="remaining">Сapacity: loading...</p>
@@ -329,15 +331,6 @@ document.addEventListener('DOMContentLoaded', async () => {
                 categoryElement.appendChild(walletElement);
             }
         }
-
-        document.querySelectorAll('.tonviewer').forEach(link => {
-            link.addEventListener('click', function() {
-                const url = this.getAttribute('data-url');
-                const target = this.getAttribute('data-target') || '_self';
-
-                window.open(url, target);
-            });
-        });
     } catch (error) {
         console.error('Error while page rendering:', error.message);
     }
